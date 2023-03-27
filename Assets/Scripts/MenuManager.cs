8b15f15b8ad3bda2f7bcf058c0970e759839ff45
KacperM45440 <104095161+KacperM45440@users.noreply.github.com>
2023-03-27 19:00:29 +0200

    Mixer glosnosci

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,12 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
+    public AudioMixer ostRef;
+    public AudioMixer sfxRef;
+    public TMP_Text ostText;
+    public TMP_Text sfxText;
+
     public void LoadGameScene()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    // Przy bardziej rozbudowanych ustawieniach ta logike rowniez mozna by bylo podpiac pod PlayerPrefs,
+    // aby zapisywac wybor gracza przy wejsciu do gry
+    private void Start()
+    {
+        sfxRef.GetFloat("SFX", out float x);
+        ostRef.GetFloat("OST", out float y);
+
+        if (x.Equals(-80))
+        {
+            sfxText.text = "SFX: OFF";
+        }
+        if (y.Equals(-80))
+        {
+            ostText.text = "OST: OFF";
+        }
+    }
+
+    public void MuteSFX()
+    {
+        sfxRef.GetFloat("SFX", out float x);
+        if (!x.Equals(-80))
+        {
+            sfxRef.SetFloat("SFX", -80);
+            sfxText.text = "SFX: OFF";
+        }
+        else
+        {
+            sfxRef.SetFloat("SFX", 0);
+            sfxText.text = "SFX: ON";
+        }
+    }
+
+    public void MuteOST()
+    {
+        ostRef.GetFloat("OST", out float x);
+        if (!x.Equals(-80))
+        {
+            ostRef.SetFloat("OST", -80);
+            ostText.text = "OST: OFF";
+        }
+        else
+        {
+            ostRef.SetFloat("OST", 0);
+            ostText.text = "OST: ON";
+        }
     }
 }
